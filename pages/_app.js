43e4cb34c80ee87f6dd9be22bd8c2a29700638dc
XsidX <developer.sid@outlook.com>
2022-12-26 12:33:35 +0300

    Setup linters

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
+
+export default MyApp;
