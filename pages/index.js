85d587eb21f80d9e0d929c814411486dfe0b9f22
XsidX <developer.sid@outlook.com>
2023-01-28 08:52:11 +0300

    Add skills section

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,25 @@ import Image from 'next/image';
 import Layout from '../components/layouts/article';
 import profilePic from '../public/me.jpg';
 import SectionHeading from '../components/SectionHeading';
+import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import ShadowBlock from '../components/ShadowBlock';
 
 const Home = () => (
   <Layout>
-    <div className="rounded-lg mb-6 p-3 text-center bg-white dark:bg-zinc-800 backdrop-filter backdrop-blur-sm bg-opacity-50 dark:bg-opacity-50 transition duration-500 ease-in-out">
+    <div className="rounded-lg mb-6 p-3 text-center bg-white dark:bg-zinc-800 dark:text-zinc-100 backdrop-filter backdrop-blur-sm bg-opacity-50 dark:bg-opacity-50 transition duration-500 ease-in-out">
       Hello, I&apos;m a full-stack web developer with a focus on remote work.
     </div>
 
     <div className="flex flex-col items-center md:flex-row">
-      <div className="self-start">
-        <h2 className="text-3xl  font-semibold font-mplus text-zinc-900 dark:text-zinc-50 transition duration-500 ease-in-out">
+      <div className="self-start text-zinc-900 dark:text-zinc-100 transition duration-500 ease-in-out">
+        <h2 className="text-3xl  font-semibold font-mplus">
           Sidney Kaguli
         </h2>
         <p> Digital Craftsman ( Designer / Developer ) </p>
       </div>
       <div className="mt-4 md:mt-0 md:ml-6">
-        <div className="border-gray-300 border-2 rounded-full w-24 h-24 inline-block overflow-hidden">
+        <div className="border-teal-900/40 border-2 rounded-full w-24 h-24 inline-block overflow-hidden">
           <Image
             src={profilePic}
             alt="Sidney Kaguli"
@@ -32,13 +33,19 @@ const Home = () => (
     </div>
     <section className="mt-6">
       <SectionHeading>Work</SectionHeading>
-      <p className="mb-6 dark:text-zinc-50 text-zinc-900 transition duration-500 ease-in-out">
+      <p className="mb-6 dark:text-zinc-100 text-zinc-900 transition duration-500 ease-in-out">
         &nbsp;&nbsp;&nbsp;I am a freelance and a full-stack developer from Nairobi, Kenya
         with a passion for building digital services. He has a knack
         for all things launching products, from planning and designing all the
         way to solving real-life problems with code. With me on your team, you can be sure
         that your product will be built with the best practises in mind and delivered on time.
       </p>
+    </section>
+    <section className="mt-6">
+      <SectionHeading>What I can do</SectionHeading>
+      <Skills />
+    </section>
+    <section>
       <div className="relative">
         <SectionHeading overlay className="text-4xl md:text-8xl text-center">Projects</SectionHeading>
         <ShadowBlock />
@@ -54,13 +61,13 @@ const Home = () => (
         <p> And that&apos;s a wrap! </p>
         <p>
           <span> I look foward to </span>
-          <span className="bg-clip-text text-transparent bg-gradient-to-b from-purple-400 to-purple-600 transition duration-500 ease-in-out">
+          <span className="bg-clip-text text-transparent bg-gradient-to-b from-teal-400 to-teal-600 transition duration-500 ease-in-out">
             chatting with you soon.
           </span>
         </p>
         <Link
           href="mailto:sidneykaguli@gmail.com"
-          className="relative mt-6 bg-zinc-900 dark:bg-zinc-50 text-zinc-50 dark:text-zinc-900 p-3 px-6 text-sm font-mplus rounded-lg shadow-md hover:shadow-zinc-900/20 dark:hover:shadow-zinc-200/20 hover:shadow-lg transition ease-in duration-100"
+          className="relative mt-6 bg-teal-900 text-zinc-50 p-3 px-6 text-sm font-mplus rounded-lg shadow-md hover:shadow-zinc-900/20 dark:hover:shadow-teal-200/20 hover:shadow-lg transition ease-in duration-100"
         >
           Email Me
         </Link>
